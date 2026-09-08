87919c828e73d49cdbcf51967b96552ac36d5557
Educatori <educatori127@gmail.com>
2026-09-08 13:11:50 +0200

    Update permessi.js

--- js/permessi.js
+++ js/permessi.js
@@ -5,7 +5,8 @@
 // x. ANAGRAFICA STUDENTI CONVITTORI ED ESTERNI
 
 const LAB_PRANZO = { 
-    1: ['4A'], 2: ['1B', '3A'], 3: ['1A', '4B'], 4: ['3B', '3C'] 
+    
+    1: ['3A'], 2: ['5A', '5B'], 3: ['1A','4B'], 4: ['1B', '4C']
 };
 
 
@@ -17,68 +18,27 @@ const TURNI_DINNER = {
 
  // CONFIGURAZIONE CLASSI LAB DINNER
 const LAB_DINNER={ 
-     1:['2P'], 2:['5A','5B'], 3:['2B'], 4:['2A']
+   
+
+    1": ['2P'], 2: ['2A'], 3: ['2B'], 4: ['5A', '5B']
 };
 
 
 
 // 1. TABELLA OVERRIDE TURNI DINNER (La modifica suggerita)
-// Formato: "COGNOME": { GiornoSettimana: TurnoDestinazione }
+// Formato: "COGNOME": { GiornoSettimana: TurnoDestinazione } "TEBANO": { 1: 2, 2: 2, 3: 2, 4: 2, 5: 2 }, // Sempre turno 2
 // GiornoSettimana: 1=Lun, 2=Mar, 3=Mer, 4=Gio, 5=Ven
 const OVERRIDE_TURNI_DINNER = {
-    "TEBANO": { 1: 2, 2: 2, 3: 2, 4: 2, 5: 2 }, // Sempre turno 2
-    "SCHIRRU": { 1: 2, 3: 2, 4: 2 },            // Turno 2 Lun, Mer, Gio
-    "BOMBONATO": { 1: 2, 3: 2, 4: 2 },
-    "PAONESSA": { 1: 2, 3: 2, 4: 2 },
-    "SALVI": { 1: 2, 3: 2 },
-    "SAITTA": { 3: 2 }                          // Turno 2 solo Mercoledì
+    
+   
 };
 
 // 2. PERMESSI PERMANENTI (Dettaglio Orari)
     const ORARI_PP = { 
         // Configurazione orari PP predefiniti (Giorno: 1=Lun, 2=Mar, 3=Mer, 4=Gio)
-    // AGGIUNGERE QUI ALTRI PP 
-    "BERRUTI A": { 4: { out: "13:30", in: "NO rientro" }},     
-    "BERRUTI S": { 3: { out: "14:00", in: "NO rientro" }},
-    "BOMBONATO": { 1: { out: "17:00", in: "18:45" }, 3: { out: "17:00", in: "18:45" }, 4: { out: "17:00", in: "18:45" }},    
-    "BOGGIA" : { 4: { out: "16:00", in: "19:00" }},
-    "BUZZI": { 1: { out: "16:40", in: "18:00" }, 2: { out: "15:50", in: "18:00" }, 3: { out: "14:00", in: "16:30" }},
-    "CASALICCHIO": { 1: { out: "18:00", in: "21:30" }, 2: { out: "18:00", in: "21:30" }, 3: { out: "18:00", in: "21:30" }, 4: { out: "18:00", in: "21:30" }},
-    "CATTARINUSSI": { 2: { out: "15:00", in: "16:30" }, 3: { out: "15:15", in: "16:45" }, 4: { out: "17:30", in: "19:00" }},
-    "CHEN": { 2: { out: "16:30", in: "NO rientro" }, 4: { out: "16:30", in: "NO rientro" }},
-    "CHESSA": { 1: { out: "17:00", in: "21:30" }, 3: { out: "17:00", in: "21:30" }, 4: { out: "17:00", in: "21:30" }}, 
-    "CHIADO": { 1: { out: "16:30", in: "18:45" }, 2: { out: "16:30", in: "18:45" }, 3: { out: "16:30", in: "18:45" }, 4: { out: "16:30", in: "18:45" }},
-    "CIRINA": { 4: { out: "16:00", in: "19:00" }}, 
-    "CLERIN": { 2: { out: "18:00", in: "21:30" }, 4: { out: "18:00", in: "21:30" }}, 
-    "COMIOTTO": { 1: { out: "20:00", in: "21:30" }, 2: { out: "16:00", in: "19:00" }, 3: { out: "17:00", in: "19:00" }, 4: { out: "16:00", in: "19:00" }},
-    "COMMOD": { 1: { out: "16:40", in: "21:30" }},  
-    "DAGOSTINO": { 3: { out: "16:00", in: "21:30" }, 4: { out: "15:00", in: "NO rientro" }},
-    "DATTRINO":  { 1: { out: "17:00", in: "18:30" }, 3: { out: "17:00", in: "18:30"}, 4: { out: "17:00", in: "18:30" }}, 
-    "DI TRIA": { 1: { out: "16:40", in: "19:00" },  2: { out: "15:50", in: "19:00" }, 3: { out: "16:40", in: "19:00" }}, 
-    "DUBLANC": { 1: { out: "17:00", in: "19:00" },  2: { out: "16:00", in: "18:00" }, 4: { out: "15:00", in: "17:00" }},
-    "EPICOCO": { 2: { out: "17:40", in: "20:00" },  3: { out: "17:40", in: "20:00" }, 4: { out:  "17:40", in: "20:00" }},
-    "FLORIAN":  { 1: { out: "16:50", in: "19:00" }, 2: { out: "15:00", in: "18:00" }, 4: { out: "16:50", in: "19:00" }},
-    "GASPARD":  { 2: { out: "17:15", in: "20:00" },  4: { out: "17:15", in: "20:00" }},  
-    "GIOVANNELLI P": { 3: { out: "17:30", in: "21:30" }},
-    "LAZIER": { 2: { out: "17:00", in: "20:00" }},
-    "LUNARDI":  { 2: { out: "17:00", in: "20:00" },  4: { out: "17:00", in: "20:00" }},     
-    "MARANGELO": { 1: { out: "17:00", in: "19:00" },  2: { out: "14:45", in: "17:00" },  4: { out: "16:00", in: "19:00" }},   
-    "MENALDINO":  { 1: { out: "18:00", in: "20:15" },   4: { out: "18:00", in: "20:15" }}, 
-    "MEYNET":  { 1: { out: "17:00", in: "19:15" }, 2: { out: "17:00", in: "19:15" }, 3: { out: "17:00", in: "19:15" }, 4: { out: "17:00", in: "19:15" }},
-    "MERLET":  { 1: { out: "16:00", in: "18:00" }, 2: { out: "16:00", in: "18:00" }, 3: { out: "16:00", in: "18:00" }, 4: { out: "16:00", in: "18:00" }},  
-    "OBERT": { 1: { out: "16:00", in: "19:15" }, 2: { out: "16:00", in: "19:15" }, 3: { out: "16:00", in: "19:15" }, 4: { out: "16:00", in: "19:15" }},
-    "OBERTO":  { 1: { out: "16:00", in: "19:00" }, 2: { out: "16:00", in: "19:00" }, 3: { out: "16:00", in: "19:00" }, 4: { out: "16:00", in: "19:00" }},   
-    "PAONESSA": { 1: { out: "16:30", in: "19:00" }, 2: { out: "16:30", in: "21:30" }, 3: { out: "16:30", in: "19:00" }, 4: { out: "16:30", in: "19:00" }},    
-    "PETRIS":  { 1: { out: "15:40", in: "19:00" }, 3: { out: "15:40", in: "19:00" }, 4: { out: "15:40", in: "19:00" }},  
-    "PIGNATELLI":  { 1: { out: "17:00", in: "20:00" }, 3: { out: "17:00", in: "20:00" }}, 
-    "QUERIO GIANETTO":  { 1: { out: "17:00", in: "19:30" }, 2: { out: "17:00", in: "19:30" }, 3: { out: "17:00", in: "19:30" }, 4: { out: "17:00", in: "19:30" }},   
-    "SAITTA": { 1: { out: "19:20", in: "20:40" }, 3: { out: "15:45", in: "18:45" }},   
-    "SALVI":  { 1: { out: "17:00", in: "19:00" },  3: { out: "17:00", in: "19:00" }, 4: { out: "17:00", in: "19:00" }},
-    "SCHIRRU":  { 1: { out: "16:40", in: "18:30" },   2: { out: "16:40", in: "18:30" },  3: { out: "16:40", in: "18:30" }},
-    "SORRENTI":  { 1: { out: "18:00", in: "19:00" }, 2: { out: "18:00", in: "19:00" }, 3: { out: "18:00", in: "19:00" }, 4: { out: "18:00", in: "19:00" }},
-    "TAPPELLA":  { 1: { out: "17:00", in: "19:00" },  2: { out: "16:00", in: "18:00" },  3: { out: "17:00", in: "19:00" },  4: { out: "16:00", in: "18:00" }},  
-    "TESSARIN": { 1: { out: "16:40", in: "NO rientro" }, 3: { out: "16:40", in: "NO rientro" }},
-    "VILARDO":  { 1: { out: "17:00", in: "18:30" }, 3: { out: "17:00", in: "18:30" }, 4: { out: "17:00", in: "18:30" }},
+    // AGGIUNGERE QUI ALTRI PP "BERRUTI A": { 4: { out: "13:30", in: "NO rientro" }}
+        
+   
 };
 
  
@@ -86,18 +46,49 @@ const OVERRIDE_TURNI_DINNER = {
 
 // 3. ASSENTI PERMESSO DINNER (Chi non cena per giorno)
  const ASSENTI_PERMESSO = { 
-    1: ['TESSARIN', 'CASALICCHIO', 'PIGNATELLI', 'MENALDINO', 'CHESSA', 'QUERIO GIANETTO'], 
-    2: ['CHEN', 'COMMOD', 'CASALICCHIO', 'CLERIN', 'LAZIER', 'LUNARDI', 'GASPARD', 'PAONESSA', 'QUERIO GIANETTO', 'BOMBONATO', 'SCHIRRU', 'VILARDO'], 
-    3: ['BERRUTI S', 'DAGOSTINO', 'GIOVANNELLI P', 'CASALICCHIO', 'SAITTA', 'PIGNATELLI', 'CHESSA', 'QUERIO GIANETTO', 'BOMBONATO', 'SCHIRRU', 'VILARDO'], 
-    4: ['BERRUTI A', 'CHEN', 'CASALICCHIO', 'CLERIN', 'DAGOSTINO', 'MENALDINO', 'CHESSA', 'QUERIO GIANETTO', 'BOMBONATO', 'SCHIRRU', 'VILARDO'] 
+   // 1: ['TESSAR]
+    
 };
 
 // 4. CALENDARIO LABORATORI DINNER (G1/G2 per 5A e 5B)
 const CALENDARIO_GRUPPI_DINNER = { 
          
-     "12/05/2026": "gr1", "14/05/2026": "gr1",
-    "19/05/2026": "gr2", "21/05/2026": "gr2", "26/05/2026": "gr1", "28/05/2026": "gr1",
-    "04/06/2026": "gr2", "09/06/2026": "gr2"
+    "17/09/2026": "gr2",
+    "24/09/2026": "gr1",
+    "01/10/2026": "gr2",
+    "08/10/2026": "gr1",
+    "01/10/2026": "gr2",
+    "08/10/2026": "gr1",
+    "15/10/2026": "gr2",
+    "22/10/2026": "gr1",
+    "29/10/2026": "gr2",
+    "05/11/2026": "gr1",
+    "12/11/2026": "gr2",
+    "19/11/2026": "gr1",
+    "26/11/2026": "gr2",
+    "03/12/2026": "gr1",
+    "10/12/2026": "gr2",
+    "17/12/2026": "gr1",
+    "07/01/2027": "gr2",
+    "14/01/2027": "gr1",
+    "21/01/2027": "gr2",
+    "28/01/2027": "gr1",
+    "04/02/2027": "gr2",
+    "18/02/2027": "gr2",
+    "25/02/2027": "gr1",
+    "04/03/2027": "gr2",
+    "11/03/2027": "gr1",
+    "18/03/2027": "gr2",
+    "01/04/2027": "gr2",
+    "08/04/2027": "gr1",
+    "15/04/2027": "gr2",
+    "22/04/2027": "gr1",
+    "29/04/2027": "gr2",
+    "06/05/2027": "gr1",
+    "13/05/2027": "gr2",
+    "20/05/2027": "gr1",
+    "27/05/2027": "gr2",
+    "03/06/2027": "gr1"
 };
 
  
